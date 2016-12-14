93662fd39a964673eda2b3c1ecfb92ddc9ffb1bc
ValentineCats <pyrostheotherfiremage@gmail.com>
2016-12-13 22:41:16 -0500

    movement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -3,23 +3,17 @@ using System.Collections;
 
 public class PlayerMovement : MonoBehaviour {
 	public float speed = 2.0f;
-	private GameObject[] directionals;
+	//private GameObject[] directionals;
 	private string[] waypoints;
 	public float moveSpeed;
 
-	void Awake (){
-		directionals = GameObject.FindGameObjectsWithTag("directional");
-	}
-
-
-
-
-
-
-
-	/*void Update(){
+	//void Awake (){
+	//	directionals = GameObject.FindGameObjectsWithTag("directional");
+	//}
+		
+	void Update(){
 		Vector3 move = new Vector3 (Input.GetAxis ("Horizontal"), 0, Input.GetAxis ("Vertical"));
 		transform.position += move * speed * Time.deltaTime;
-	}*/
+	}
 
 }
