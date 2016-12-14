e1468d51ca7726b5a1a5724edcf0768b1efefb66
ValentineCats <pyrostheotherfiremage@gmail.com>
2016-12-13 22:01:23 -0500

    camera change and heart beat

--- Assets/Scripts/HeartBeat.cs
+++ Assets/Scripts/HeartBeat.cs
@@ -7,12 +7,16 @@ public class HeartBeat : MonoBehaviour {
 	private float delay;
 	private float hold;
 	private IEnumerator coroutine;
+	private BoxCollider bc;
+	private MeshRenderer mr;
 	// Use this for initialization
 	void Start () {
 		delay = 2f;
 		hold = 1f;
 		coroutine = pump ();
 		StartCoroutine (coroutine);
+		bc = this.GetComponent<BoxCollider> ();
+		mr = this.GetComponent<MeshRenderer> ();
 	}
 
 
