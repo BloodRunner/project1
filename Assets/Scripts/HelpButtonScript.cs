91bf6aacdac55e35c3e5d2310882a0c0049cb17b
Phoebe Miller <phoebe_miller@harvard.edu>
2016-12-14 21:29:00 -0500

    exclaim!

---
+++ Assets/Scripts/HelpButtonScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelpButtonScript : MonoBehaviour
+{
+	private RaycastHit hit;
+	private Ray ray;
+	Ray shootRay;
+	int buttonMask; // Only care for Organ sword/shield buttons
+	int organLayer;
+	//Color white = new Color(255,255,255);
+	//Color black = new Color(0,0,0);
+
+	void Awake ()
+	{
+		buttonMask = LayerMask.GetMask ("SwordShield") ;
+		organLayer = LayerMask.NameToLayer ("Organ");
+
+	}
+
+	// Button click
+	void flash (GameObject button) {
+		SpriteRenderer sr;
+		sr = button.GetComponent<SpriteRenderer> ();
+		StartCoroutine(FlashSprite(sr));
+	}
+
+	IEnumerator FlashSprite(SpriteRenderer sr)
+	{
+		sr.enabled = false;
+		yield return new WaitForSeconds(.2f);
+		sr.enabled = true;
+	}
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1")) {
+			Camera cx = Camera.current;
+			Camera[] cxs = Camera.allCameras;
+			if (cx == null) {
+				cx = cxs [0]; 
+			}
+			Ray ray = cx.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+			string callMesg = "";
+		
+			if (Physics.Raycast (ray, out hit, 1000, buttonMask)) {
+				//Debug.Log ("Help Button Script" + hit.collider.tag);
+				if (hit.collider.tag.Equals ("UI")) {
+					GameObject button = hit.collider.gameObject;
+					if (button.name.Equals("CallButton")) 
+						callMesg="callForSupport";
+					else
+						Debug.Log ("button " + button.name + " fired for support ");
+					GameObject parent = button.transform.parent.gameObject;
+					//Debug.Log ("Hit organ " + parent.name +" layer "+ parent.layer);
+					if (parent.layer == organLayer) {				
+						parent.SendMessage (callMesg);
+						Debug.Log ("Hit organ " + parent.name+ " "+ callMesg);
+					}
+					flash (button);
+				}
+			}
+		}
+	}
+			
+}
