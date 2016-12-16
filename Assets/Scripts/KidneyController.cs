99c6047ed310d3d6e9f31632e2b83bf0020341a2
Phoebe Miller <phoebe_miller@harvard.edu>
2016-12-15 19:17:17 -0500

    red speed

--- Assets/Scripts/KidneyController.cs
+++ Assets/Scripts/KidneyController.cs
@@ -9,9 +9,9 @@ public class KidneyController : OrganController {
 	public override void damageBody(){
 		Debug.Log (name +" damageBody " + bodystate.showStats ());
 		if (otherKidney == null || !ReferenceEquals (otherKidney, null)) {
-			bodystate.updateRedSpeedStats (stats_health); // Only one kidney
+			bodystate.updateRedSpeedStats (stats_health + 1f); // Only one kidney
 		} else {
-			bodystate.updateRedSpeedStats( otherKidney.get_stats_power() + stats_health); // 2 kidneys
+			bodystate.updateRedSpeedStats( 1f + otherKidney.get_stats_power() + stats_health); // 2 kidneys
 		}
 
 	}
